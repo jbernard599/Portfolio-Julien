348e7612dc53f4c6363e456df601d554301d0791
Bernard Julien <julien.bernard599@gmail.com>
2024-01-06 19:52:28 +0100

    Ajout global du Portfolio

---
+++ js/Jquery.js
@@ -0,0 +1,23 @@
+// window.onload = function() {
+    // var itemsToShow = 9; // Nombre d'éléments à afficher initialement
+    // var portfolioItems = Array.from(document.querySelectorAll(".portfolio-wrapper .grid-item"));
+    // var loadMoreButton = document.querySelector(".load-more a");
+
+    Affiche initialement les éléments désignés
+    // portfolioItems.slice(0, itemsToShow).forEach(function(item) {
+        // item.classList.remove("hidden");
+    // });
+
+    // loadMoreButton.addEventListener("click", function(e) {
+        // e.preventDefault();
+
+        Affiche tous les éléments restants lorsque "Voir plus" est cliqué
+        // var hiddenItems = Array.from(document.querySelectorAll(".portfolio-wrapper .grid-item.hidden"));
+        // hiddenItems.forEach(function(item) {
+            // item.classList.remove("hidden");
+        // });
+
+        Cachez le bouton "Voir plus" après le clic
+        // loadMoreButton.style.display = 'none';
+    // });
+// }
